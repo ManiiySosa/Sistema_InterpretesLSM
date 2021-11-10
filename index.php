bcb7d362cc3e74cf4715bdbdc2baf230d5131203
Manuel Antonio <zS15013926@estudiantes.uv.mx>
2021-11-10 18:56:57 +0100

    segundo commit, maquetacion contenido central interpretes

--- index.php
+++ index.php
@@ -80,15 +80,67 @@
             </div>
           </div>
         </div>
+        <a href="#">Gestionar Servicio</a>
+        <a href="#">Valoraciones</a>
       </aside>
       <!-- Fin Barra Lateral -->
       <!-- seccion central -->
       <section id="central">
         <h1>Interpretes destacados</h1>
-        <article class="profile">
+          <article class="service">
+            <div class="service-info">
+              <p>
+                <label for="">Nombre:</label><span></span>
+              </p>
+              <p>
+                <label for="">Area Especialidad:</label><span></span>
+              </p>
+             <p>
+                <label for="">Años experiencia en LSM:</label><span></span>
+             </p>
+              <p>
+                <label for="">Correo electronico:</label><span></span>
+              </p>
+             <p>
+                <label for="">Telefono:</label><span></span>
+             </p>
+              <p>
+                <label for="">Valoracion </label><span></span>
+              </p>
+              <a href="#">Calificar Interprete</a>
 
+            </div>
+            <div class="video-interprete">
+              <video src="assets/video/hola-man.mp4" id="video-interprete" controls muted loop></video>
+            </div>
+          </article>
+          <article class="service">
+            <div class="service-info">
+              <p>
+                  <label for="">Nombre:</label><span></span>
+                </p>
+                <p>
+                  <label for="">Area Especialidad:</label><span></span>
+                </p>
+              <p>
+                  <label for="">Años experiencia en LSM:</label><span></span>
+              </p>
+                <p>
+                  <label for="">Correo electronico:</label><span></span>
+                </p>
+              <p>
+                  <label for="">Telefono:</label><span></span>
+              </p>
+                <p>
+                  <label for="">Valoracion </label><span></span>
+                </p>
+                <a href="#">Calificar Interprete</a>
 
-        </article>
+            </div>
+            <div class="video-interprete">
+              <video src="assets/video/hola-man.mp4" id="video-interprete" controls muted loop></video>
+            </div>
+          </article>
       </section>
       <!--Fin seccion central -->
     </div>
